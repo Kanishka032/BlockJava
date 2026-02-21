e4fa5557b98193e889418221e3b65ffdb472c283
Kanishka Gupta <162425328+Kanishka032@users.noreply.github.com>
2026-02-21 13:33:19 +0530

    Delete frontend directory

--- frontend/src/pages/ProjectRoute.tsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
-  const student = localStorage.getItem("student");
-  return student ? children : <Navigate to="/login" replace />;
-};
-
-export default ProtectedRoute;
